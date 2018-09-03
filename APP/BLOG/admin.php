d6c57da2fbffb9d92f0912ee426462fdf6ca616f
space2pacman <space2pacman@gmail.com>
2018-09-03 12:09:00 +0300

    add HOMEWORK reload BLOG

--- APP/BLOG/admin.php
+++ APP/BLOG/admin.php
@@ -6,8 +6,9 @@ include("db.php");
 if(isset ($_SESSION['user'])) {
     echo "Привет ".$_SESSION['user'];
     echo '
-		<form action="" method="POST">
+		<form action="" method="POST" enctype="multipart/form-data">
 		    <input type="text" name="header" placeholder="заголовок" style="margin-bottom: 5px;"></br>
+		    <input type="file" name="pic"><br>
 		    <textarea name="text" cols="30" placeholder="введите текст..." rows="10"></textarea></br>
 		    <input type="submit" value="Отправить" style="margin-top: 5px;">
 		</form>
@@ -21,10 +22,19 @@ if(isset ($_SESSION['user'])) {
 
 
 <?php
+$file = $_FILES["pic"];
+
+if(!$file["name"] == "") {
+	$link = "img/".$file["name"];
+	move_uploaded_file($file["tmp_name"], $link);
+	$link = "'".$link."'";
+} else {
+	$link = "NULL";
+}
 
 $header = $_POST['header'];
 $content = $_POST['text'];
-if (!empty($header) && !empty($content))
-    mysql_query("insert into content (header, content) values ('$header', '$content')");
-
+if (!empty($header) && !empty($content)) {
+    mysql_query("insert into content (header, pic, content) values ('$header', $link, '$content')");
+}
 ?>
