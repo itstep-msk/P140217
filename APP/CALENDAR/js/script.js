ddbc01c7b90c11f4f61e32387b841e3aa44a8820
space2pacman <space2pacman@gmail.com>
2018-10-16 12:22:36 +0300

    update CALENDAR APP

--- APP/CALENDAR/js/script.js
+++ APP/CALENDAR/js/script.js
@@ -1,7 +1,19 @@
 var date = new Date();
+var DB = {};
 var days = $(".days");
+var weekdays = $(".weekdays");
+var popup = {
+	parent: $(".popup"),
+	caption: $(".popup-caption"),
+	close: $(".popup-close"),
+	name: $(".popup-name"),
+	text: $(".popup-text"),
+	button: $(".popup-button"),
+}
 var template = {
-	day: "<div class='day'></div>"
+	day: "<div class='day'></div>",
+	weekday: "<div class='weekday-item'></div>",
+	empty: "<div class='empty'></div>"
 }
 var el = {
 	month: {
@@ -14,10 +26,37 @@ var names = {
 	months: ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
 	weekdays: ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
 }
+// POPUP
+popup.close.on("click", function() {
+	popup.parent.hide();
+})
+
+popup.button.on("click", function() {
+	console.log(popup.caption.html() + ":" + popup.name.val() + ":" + popup.text.val());
+	addToDB(date.getFullYear(),date.getMonth(),popup.parent.attr("data-day"), popup.name.val() + ":" + popup.text.val())
+	console.log(DB)
+})
+// /POPUP
+
+days.on("click", ".day", function() {
+	popup.caption.html(date.getFullYear() + "/" + date.getMonth() + "/" + $(this).attr("data-day"))
+	popup.parent.show();
+	popup.parent.attr("data-day", $(this).attr("data-day"))
+})
 
 Date.prototype.daysInMonth = function() {
 	return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
 };
+
+function addToDB(year, month, day, data) {
+	if(!(year in DB)) {
+		DB[year] = {};
+	}
+	if(!(month in DB[year])) {
+		DB[year][month] = {};
+	}
+	DB[year][month][day] = data;
+}
 
 function selectThisDay(){
 	$(".day[data-day='" + date.getDate() + "']").addClass("today")
@@ -39,13 +78,24 @@ function createMonth() {
 }
 
 function addingDays() {
+	for(var i = 0; i < date.getDay(); i++) {
+		days.append(template.empty);
+	}
+
 	for(var i = 1; i <= date.daysInMonth(); i++) {
 		days.append(template.day).find(".day").last().html(i).attr("data-day",i);
 	}
 }
 
+function addingWeekdays() {
+	for(var i = 0; i < 7; i++) {
+		weekdays.append(template.weekday).find(".weekday-item").last().html(names.weekdays[i]);
+	}
+}
+
 function init() {
 	addingDays();
+	addingWeekdays();
 	createMonth();
 	selectThisDay();
 }
